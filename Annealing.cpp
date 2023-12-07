471e3c8ff08e485771d9a9f67c3ea5830574658a
CristobalTorresU <cristobal.torres.u@usach.cl>
2023-12-07 00:41:52 -0300

    owo

--- Annealing.cpp
+++ Annealing.cpp
@@ -10,13 +10,61 @@ Annealing::Annealing (MatrizCosto* matriz, int maxIter, vector<int> tour, double
 		if (new_cost < cost) {
 			tour = new_tour;
 			cost = new_cost;
-            		//cout << iter << ") Cost: " << cost << endl;
-        	} else {
-            		//cout << iter << ") No improvement: " << cost << endl;
-        	}
-        	iter++;
-    	}
+            //cout << iter << ") Cost: " << cost << endl;
+        } else {
+           	//cout << iter << ") No improvement: " << cost << endl;
+        }
+		iter++;
+    }
+	iter = 0;
+	while (iter < maxIter) {
+		vector<int> new_tour(aleatoriedad(tour));
+		double new_cost = tour_cost(new_tour);
+		if (new_cost < cost) {
+			tour = new_tour;
+			cost = new_cost;
+            cout << iter << ") Cost: " << cost << endl;
+		} else {
+           	cout << iter << ") No improvement: " << cost << endl;
+		}
+		iter++;
+	}
 	cout << "Costo Annealing = " << cost << endl;
+}
+
+vector<int> Annealing::aleatoriedad (vector<int> tour) {
+	vector<int> new_tour(tour);
+	double** c = this->matrizCostos->getMatriz();
+	int n = this->matrizCostos->getSize();
+    for(int i = 0 ; i < n - 1 ; i++) {
+        for(int j = i + 1 ; j < n ; j++) {
+            // iteramos sobre todos los pares de aristas (i,i+1) y (j,j+1)
+
+            // se destruye la aristas: (i,i+1) (j,j+1)
+            double current_cost = c[new_tour[i]][new_tour[i+1]] + c[new_tour[j]][new_tour[(j+1)%n]];
+            // se crea la aristas: (i,j) (i+1,j+1)
+            double new_cost = c[new_tour[i]][new_tour[j]] + c[new_tour[i+1]][new_tour[(j+1)%n]];
+
+            
+            if(new_cost < current_cost) {// va a bajar el costo total del tour
+                // se destruye la aristas: (i,i+1) (j,j+1)
+                // se crea la aristas: (i,j) (i+1,j+1)
+                // --> se invierte el orden de los nodos entre i+1 y j
+                int k = i+1;
+                int l = j;
+                while(k < l) {
+                    int temp = new_tour[k];
+                    new_tour[k] = new_tour[l];
+                    new_tour[l] = temp;
+                    k++;
+                    l--;
+                }
+                // retornamos el primer menor tour que encontramos
+                return(new_tour);
+            }
+        }
+    }
+    return(new_tour);
 }
 
 vector<int> Annealing::two_opt_first(vector<int> tour) {
